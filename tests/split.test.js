2fee4ed49154e9804218fbc4dfec5ffde7f7c54f
tikature <detikaa10@gmail.com>
2025-11-12 17:20:26 +0700

    test: add additional test cases for splitBill and calculateTotalWithCharge functions

--- tests/split.test.js
+++ tests/split.test.js
@@ -1,6 +1,16 @@
 const { splitBill, calculateTotalWithCharge } = require("../src/split.js");
 
-test("splitBill membagi tagihan secara rata (belum ", () => {
+test("splitBill membagi tagihan secara rata (belum pembulatan)", () => {
     const result = splitBill(20000, 4);
     expect(result).toBe(5000);
+});
+
+test("splitBill membagi tagihan dengan pembulatan 1000", () => {
+    const result = splitBill(21000, 4, 1000);
+    expect(result).toBe(6000);
+});
+
+test("calculateTotalWithCharge menghitung total dengan service fee dan diskon", () => {
+    const result = calculateTotalWithCharge(200000, 10000, 20000);
+    expect(result).toBe(190000);
 });
