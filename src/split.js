bf0c3abab82fe2c932d0749e31e243fb0b6607b6
tikature <detikaa10@gmail.com>
2025-11-12 17:19:48 +0700

    feat: enhance splitBill function to support rounding and add calculateTotalWithCharge function

--- src/split.js
+++ src/split.js
@@ -1,6 +1,12 @@
-function splitBill(total, people) {
+function splitBill(total, people, roundTo = 100) {
     if (people <= 0) return 0;
-    return total / people;
+    const raw = total / people;
+    return Math.ceil(raw / roundTo) * roundTo;
 }
 
-module.exports = { splitBill };
+function calculateTotalWithCharge(total, serviceFee = 5000, discount = 0) {
+    const afterDiscount = total - discount;
+    return afterDiscount + serviceFee;
+}
+
+module.exports = { splitBill, calculateTotalWithCharge };
--- tests/split.test.js
+++ tests/split.test.js
@@ -1,6 +1,6 @@
-const { splitBill } = require("../src/split.js");
+const { splitBill, calculateTotalWithCharge } = require("../src/split.js");
 
-test("splitBill membagi tagihan secara rata", () => {
+test("splitBill membagi tagihan secara rata (belum ", () => {
     const result = splitBill(20000, 4);
     expect(result).toBe(5000);
 });
